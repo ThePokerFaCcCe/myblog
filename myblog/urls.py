f3b93ec2e8f2e1e65172f3dbc5a036f8be44fac0
ThePokerFaCcCe <thepokerfa3e@gmail.com>
2021-10-31 17:27:48 +0330

    :sparkles: Add Swagger url

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,13 +1,15 @@
-from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('api/v1/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path("api/v1/docs/", SpectacularSwaggerView.as_view(url_name='schema')),
+    path('api/v1/', include("social.urls")),
 ]
 
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns += [
-        path(r'^__debug__/', include(debug_toolbar.urls)),
+        path('__debug__/', include(debug_toolbar.urls)),
     ]
