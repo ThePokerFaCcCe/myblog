8f7502e102bb4c42c2ceeccf7c8914ff30cfbf1f
ThePokerFaCcCe <thepokerfa3e@gmail.com>
2021-11-02 00:23:18 +0330

    :sparkles: Add signals for creating slug

---
+++ blog/signals.py
@@ -0,0 +1,32 @@
+from django.db.models.signals import pre_save
+from django.utils.text import slugify
+from django.dispatch import receiver
+
+from .models import Category, Post
+
+
+def _create_slug_from_title(model, instance):
+    title = instance.title
+
+    slug = slugify(title, allow_unicode=True)
+    i = 1
+
+    while model.objects.filter(slug=slug).exists():
+        slug = slugify(f"{title}-{i}", allow_unicode=True)
+        i += 1
+
+    instance.slug = slug
+
+
+@receiver(pre_save, sender=Category)
+def create_slug_from_title_category(sender, *args, **kwargs):
+    instance = kwargs['instance']
+    if not instance.slug:
+        _create_slug_from_title(Category, instance)
+
+
+@receiver(pre_save, sender=Post)
+def create_slug_from_title_post(sender, *args, **kwargs):
+    instance = kwargs['instance']
+    if not instance.slug:
+        _create_slug_from_title(Post, instance)
