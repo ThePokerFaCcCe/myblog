73ed770eb0f162f8f13eb87673064e3149360ad3
ThePokerFaCcCe <ThePokerfa3e@gmail.com>
2021-11-05 00:11:50 +0330

    :heavy_check_mark: Add test for user profile image

--- blog/tests.py
+++ blog/tests.py
@@ -1,9 +1,12 @@
-from django.urls.base import reverse
-from rest_framework import status
 from rest_framework.test import APIClient
+from rest_framework import status
 from django.utils.text import slugify
+from django.urls.base import reverse
 from django.test import TestCase
 from uuid import uuid4
+from PIL import Image
+import tempfile
+import os
 
 from .models import Category, Post, User
 
@@ -50,6 +53,9 @@ class CreateSlugTest(TestCase):
 class UserTest(TestCase):
     def _user_url(self, pk):
         return reverse("blog:users-detail", args=[pk])
+
+    def _user_profile_url(self, pk):
+        return reverse("blog:users-profile-image", args=[pk])
 
     def setUp(self) -> None:
         self.superuser = create_user(is_superuser=True, is_staff=True)
@@ -121,3 +127,28 @@ class UserTest(TestCase):
         self.assertEqual(user2.is_vip, True)
         self.assertEqual(user2.is_staff, True)
         self.assertEqual(user2.is_superuser, True)
+
+    def test_upload_profile_image(self):
+        with tempfile.NamedTemporaryFile(suffix='.jpg') as f:
+            img = Image.new('RGB', (10, 10))
+            img.save(f, format='JPEG')
+            f.seek(0)  # Set pointer at first of file
+            res = self.user_client.post(
+                self._user_profile_url(self.user.pk),
+                data={'profile_image': f},
+                format='multipart')
+
+        self.user.refresh_from_db()
+        self.assertEqual(res.status_code, status.HTTP_200_OK, msg=res.data)
+        self.assertIn('profile_image', res.data)
+        self.assertTrue(os.path.exists(self.user.profile_image.image.path))
+        self.user.profile_image.delete()
+
+    def test_delete_profile_by_other(self):
+        user2 = create_user()
+        res = self.user_client.delete(
+            self._user_profile_url(user2.pk),
+            data=self.data,
+        )
+
+        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
